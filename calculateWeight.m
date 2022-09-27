839a691380f65e4060b0e6b7897e0d43330bd2a8
Lily Jiang <91999599+ljiang09@users.noreply.github.com>
2022-09-26 22:22:48 -0400

    make sure file is up to date

--- calculateWeight.m
+++ calculateWeight.m
@@ -1,9 +1,11 @@
 % returns weight in pounds
-% takes in diameter values of inches
-% length is also in inches
-function weight = calculateWeight(D, d, L)
+% takes in diameter and length values of inches
+% density is in kg/m^3
+function weight = calculateWeight(D, d, L, density)
+    pi = 3.14159265354962;
     R = D/2;
     r = d/2;
-    pi = 3.14159265354962;
-    weight = pi*(R^2-r^2) * L;
+    density_imperial = density/27680;  % convert to imperial, lb / in^3
+
+    weight = pi*(R^2-r^2) * L * density_imperial;
 end
